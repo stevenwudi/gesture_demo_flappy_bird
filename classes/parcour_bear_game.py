fd8bd2f5249adab9649906106399890d3ca53cb4
Di Wu <stevenwudi@gmail.com>
2016-08-30 20:11:37 +0800

    parcour bear game class update

--- classes/parcour_bear_game.py
+++ classes/parcour_bear_game.py
@@ -1,14 +1,14 @@
 import pyautogui
 import cv2
-hand_pos = [250, 250, 350, 350]  # hand position
-hand_convex_number = 1
+hand_pos = [250, 300, 350, 400]  # hand position
+hand_convex_number = 2
 
 
 class ParcourBearGame:
     def __init__(self, cap, tracker):
         self.cap = cap
         self.tracker = tracker
-        self.jump_vertical_ratio = 0.01
+        self.jump_vertical_ratio = 0.008
 
     def init_parcour_game(self):
         pyautogui.moveTo(850, 770, 2, pyautogui.easeInQuad)
@@ -22,7 +22,6 @@ class ParcourBearGame:
         self.track_pos_prev = track_pos_prev
 
     def update(self, track_pos_prev):
-        # cap, tracker, track_pos_prev:
         ret, img = self.cap.read()
         self.tracker.update(img)
         pos = self.tracker.get_position()
@@ -39,10 +38,5 @@ class ParcourBearGame:
 
         cv2.rectangle(img, (int(pos.right()), int(pos.bottom())), (int(pos.left()), int(pos.top())),
                       (255, 0, 0), 0)
-        left = max(0, int(pos.left()))
-        right = min(img.shape[0], int(pos.right()))
-        top = max(0, int(pos.top()))
-        bottom = min(img.shape[1], int(pos.bottom()))
-        crop_img = img[top:bottom, left:right]
         cv2.imshow('Gesture', img)
         cv2.waitKey(1)
