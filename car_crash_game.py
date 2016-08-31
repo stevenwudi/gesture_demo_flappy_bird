ca25e0028adc350bb55c7dfb3a5dfac6d6de5894
Di Wu <stevenwudi@gmail.com>
2016-08-31 23:31:06 +0800

    body detection function added

---
+++ car_crash_game.py
@@ -0,0 +1,39 @@
+"""
+This is the demon program using body detection/tracking to control a mini game: Parcour Bear
+http://www.4399.com/flash/172170_4.htm
+author: Di Wu
+email: stevenwudi@gmail.com
+"""
+import dlib
+import cv2
+from classes.car_crash_game import CarCrashGame
+from classes.body_detect import detect_body
+from classes.car_crash_game import body_pos
+
+
+tracker = dlib.correlation_tracker()  # dlib correlation tracker initialisation
+cap = cv2.VideoCapture(0)   # capture the video using opencv video capture
+CarCrashGame = CarCrashGame(cap, tracker)
+track_flag = False  # track flag indicate whether we have a hand detected and start tracking
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+while cap.isOpened():
+    ret, img = cap.read()
+    if not track_flag:
+        body_detected = detect_body(img, body_pos, hog)
+        # if body_detected:
+        #     tracker.start_track(img, dlib.rectangle(hand_pos[0], hand_pos[1], hand_pos[2], hand_pos[3]))
+        #     track_flag = True
+        #     pos = tracker.get_position()
+        #     track_pos_prev = [(pos.left() + pos.right()) / 2., (pos.top() + pos.bottom()) / 2.]
+        #     # we start the game if there is a hand detected
+        #     CarCrashGame.start_game(track_pos_prev)
+        #     while True:
+        #         pos = tracker.get_position()
+        #         track_pos_prev = [(pos.left() + pos.right()) / 2., (pos.top() + pos.bottom()) / 2.]
+        #         CarCrashGame.update(track_pos_prev)
+
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
